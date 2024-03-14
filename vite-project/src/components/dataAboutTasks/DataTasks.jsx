e011cd590238b8f3c3d600fa08cb5285516be198
Kekyra228 <katyiv02@gmail.com>
2024-03-15 03:35:15 +0400

    вср стили

--- vite-project/src/components/dataAboutTasks/DataTasks.jsx
+++ vite-project/src/components/dataAboutTasks/DataTasks.jsx
@@ -32,9 +32,9 @@ function DataTask() {
 							</div>
 							<div className="pop-browse__wrap">
 								<htmlForm className="pop-browse__form form-browse" id="formBrowseCard" action="#">									
-									<div className="htmlForm-browse__block">
+									<div className="form-browse__block">
 										<label htmlFor="textArea01" className="subttl">Описание задачи</label>
-										<textarea className="htmlForm-browse__area" name="text" id="textArea01"  readOnly placeholder="Введите описание задачи..."></textarea>
+										<textarea className="form-browse__area" name="text" id="textArea01"  readOnly placeholder="Введите описание задачи..."></textarea>
 									</div>
 								</htmlForm>
 								<div className="pop-new-card__calendar calendar">
