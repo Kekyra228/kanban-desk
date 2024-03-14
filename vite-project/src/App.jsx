f083eb96344397e813e449a867ac84ea116edf33
Kekyra228 <katyiv02@gmail.com>
2024-03-15 03:24:18 +0400

    есть компоненты

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import IsExit from './components/exit/IsExit'
+import CreatedTask from './components/createdTask/CreatedTask'
+import DataTask from './components/dataAboutTasks/DataTasks'
+import Header from './components/header/Header'
+import MainBlock from './components/mainBlock/MainBlock'
+import CalendarContent from './components/calendar/Calendar'
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <IsExit/>
+      <CreatedTask/>
+      <DataTask/>
+      <MainBlock/>
+      
+   </>
+  )
+}
+
+export default App
